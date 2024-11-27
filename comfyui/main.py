d992f20e00eb957310f589bf2a3aa831f3de70f1
Oleksandr Ostrenko <oleksandr@ostrenko.dev>
2024-11-27 22:32:31 +0100

    Refactor main script
    * use relative imports and import some classes
    * refactor entry point code into a function

--- comfyui/main.py
+++ comfyui/main.py
@@ -1,32 +1,12 @@
-import comfyui.comfy.options
+from .comfy import options
+options.enable_args_parsing()
 
 import os
 import importlib.util
-import comfyui.folder_paths
+from . import folder_paths
 import time
-from comfyui.comfy.cli_args import args
-from comfyui.app.logger import setup_logger
-import asyncio
-import itertools
-import shutil
-import threading
-import gc
-
-import logging
-import comfyui.utils.extra_config
-
-import comfyui.comfy.utils
-
-import comfyui.execution
-import comfyui.server
-from comfyui.server import BinaryEventTypes
-import comfyui.nodes
-import comfyui.comfy.model_management
-import comfyui.cuda_malloc
-import comfyui.fix_torch
-import comfyui.new_updater
-
-comfyui.comfy.options.enable_args_parsing()
+from .comfy.cli_args import args
+from .app.logger import setup_logger
 
 
 setup_logger(log_level=args.verbose)
@@ -47,7 +27,7 @@ def execute_prestartup_script():
     if args.disable_all_custom_nodes:
         return
 
-    node_paths = comfyui.folder_paths.get_folder_paths("custom_nodes")
+    node_paths = folder_paths.get_folder_paths("custom_nodes")
     for custom_node_path in node_paths:
         possible_modules = os.listdir(custom_node_path)
         node_prestartup_times = []
@@ -76,11 +56,19 @@ execute_prestartup_script()
 
 
 # Main code
+import asyncio
+import itertools
+import shutil
+import threading
+import gc
+
+import logging
+from .utils import extra_config
 
 if os.name == "nt":
     logging.getLogger("xformers").addFilter(lambda record: 'A matching Triton is not available' not in record.getMessage())
 
-if __name__ == "__main__":
+def init_cuda_devices():
     if args.cuda_device is not None:
         os.environ['CUDA_VISIBLE_DEVICES'] = str(args.cuda_device)
         os.environ['HIP_VISIBLE_DEVICES'] = str(args.cuda_device)
@@ -90,27 +78,34 @@ if __name__ == "__main__":
         if 'CUBLAS_WORKSPACE_CONFIG' not in os.environ:
             os.environ['CUBLAS_WORKSPACE_CONFIG'] = ":4096:8"
 
-    import comfyui.cuda_malloc
+    from . import cuda_malloc
 
 if args.windows_standalone_build:
     try:
-        import comfyui.fix_torch
+        from . import fix_torch
     except:
         pass
 
+from .comfy import utils
+
+from . import execution
+from .server import BinaryEventTypes, PromptServer
+from . import nodes
+from .comfy import model_management
+
 def cuda_malloc_warning():
-    device = comfyui.comfy.model_management.get_torch_device()
-    device_name = comfyui.comfy.model_management.get_torch_device_name(device)
+    device = model_management.get_torch_device()
+    device_name = model_management.get_torch_device_name(device)
     cuda_malloc_warning = False
     if "cudaMallocAsync" in device_name:
-        for b in comfyui.cuda_malloc.blacklist:
+        for b in cuda_malloc.blacklist:
             if b in device_name:
                 cuda_malloc_warning = True
         if cuda_malloc_warning:
             logging.warning("\nWARNING: this card most likely does not support cuda-malloc, if you get \"CUDA error\" please run ComfyUI with: --disable-cuda-malloc\n")
 
 def prompt_worker(q, server):
-    e = comfyui.execution.PromptExecutor(server, lru_size=args.cache_lru)
+    e = execution.PromptExecutor(server, lru_size=args.cache_lru)
     last_gc_collect = 0
     need_gc = False
     gc_collect_interval = 10.0
@@ -131,7 +126,7 @@ def prompt_worker(q, server):
             need_gc = True
             q.task_done(item_id,
                         e.history_result,
-                        status=comfyui.execution.PromptQueue.ExecutionStatus(
+                        status=execution.PromptQueue.ExecutionStatus(
                             status_str='success' if e.success else 'error',
                             completed=e.success,
                             messages=e.status_messages))
@@ -146,7 +141,7 @@ def prompt_worker(q, server):
         free_memory = flags.get("free_memory", False)
 
         if flags.get("unload_models", free_memory):
-            comfyui.comfy.model_management.unload_all_models()
+            model_management.unload_all_models()
             need_gc = True
             last_gc_collect = 0
 
@@ -158,9 +153,9 @@ def prompt_worker(q, server):
         if need_gc:
             current_time = time.perf_counter()
             if (current_time - last_gc_collect) > gc_collect_interval:
-                comfyui.comfy.model_management.cleanup_models()
+                model_management.cleanup_models()
                 gc.collect()
-                comfyui.comfy.model_management.soft_empty_cache()
+                model_management.soft_empty_cache()
                 last_gc_collect = current_time
                 need_gc = False
 
@@ -173,83 +168,85 @@ async def run(server, address='', port=8188, verbose=True, call_on_start=None):
 
 def hijack_progress(server):
     def hook(value, total, preview_image):
-        comfyui.comfy.model_management.throw_exception_if_processing_interrupted()
+        model_management.throw_exception_if_processing_interrupted()
         progress = {"value": value, "max": total, "prompt_id": server.last_prompt_id, "node": server.last_node_id}
 
         server.send_sync("progress", progress, server.client_id)
         if preview_image is not None:
             server.send_sync(BinaryEventTypes.UNENCODED_PREVIEW_IMAGE, preview_image, server.client_id)
-    comfyui.comfy.utils.set_progress_bar_global_hook(hook)
+    utils.set_progress_bar_global_hook(hook)
 
 
 def cleanup_temp():
-    temp_dir = comfyui.folder_paths.get_temp_directory()
+    temp_dir = folder_paths.get_temp_directory()
     if os.path.exists(temp_dir):
         shutil.rmtree(temp_dir, ignore_errors=True)
 
 
-if __name__ == "__main__":
+def main():
+    init_cuda_devices()
+
     if args.temp_directory:
         temp_dir = os.path.join(os.path.abspath(args.temp_directory), "temp")
         logging.info(f"Setting temp directory to: {temp_dir}")
-        comfyui.folder_paths.set_temp_directory(temp_dir)
+        folder_paths.set_temp_directory(temp_dir)
     cleanup_temp()
 
     if args.windows_standalone_build:
         try:
-            import comfyui.new_updater
-            comfyui.new_updater.update_windows_updater()
+            from . import new_updater
+            new_updater.update_windows_updater()
         except:
             pass
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    comfyui.server = comfyui.server.PromptServer(loop)
-    q = comfyui.execution.PromptQueue(comfyui.server)
+    server = PromptServer(loop)
+    q = execution.PromptQueue(server)
 
     extra_model_paths_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extra_model_paths.yaml")
     if os.path.isfile(extra_model_paths_config_path):
-        comfyui.utils.extra_config.load_extra_path_config(extra_model_paths_config_path)
+        extra_config.load_extra_path_config(extra_model_paths_config_path)
 
     if args.extra_model_paths_config:
         for config_path in itertools.chain(*args.extra_model_paths_config):
-            comfyui.utils.extra_config.load_extra_path_config(config_path)
+            extra_config.load_extra_path_config(config_path)
 
-    comfyui.nodes.init_extra_nodes(init_custom_nodes=not args.disable_all_custom_nodes)
+    nodes.init_extra_nodes(init_custom_nodes=not args.disable_all_custom_nodes)
 
     cuda_malloc_warning()
 
-    comfyui.server.add_routes()
-    hijack_progress(comfyui.server)
+    server.add_routes()
+    hijack_progress(server)
 
-    threading.Thread(target=prompt_worker, daemon=True, args=(q, comfyui.server,)).start()
+    threading.Thread(target=prompt_worker, daemon=True, args=(q, server,)).start()
 
     if args.output_directory:
         output_dir = os.path.abspath(args.output_directory)
         logging.info(f"Setting output directory to: {output_dir}")
-        comfyui.folder_paths.set_output_directory(output_dir)
+        folder_paths.set_output_directory(output_dir)
 
     #These are the default folders that checkpoints, clip and vae models will be saved to when using CheckpointSave, etc.. nodes
-    comfyui.folder_paths.add_model_folder_path("checkpoints", os.path.join(comfyui.folder_paths.get_output_directory(), "checkpoints"))
-    comfyui.folder_paths.add_model_folder_path("clip", os.path.join(comfyui.folder_paths.get_output_directory(), "clip"))
-    comfyui.folder_paths.add_model_folder_path("vae", os.path.join(comfyui.folder_paths.get_output_directory(), "vae"))
-    comfyui.folder_paths.add_model_folder_path("diffusion_models", os.path.join(comfyui.folder_paths.get_output_directory(), "diffusion_models"))
-    comfyui.folder_paths.add_model_folder_path("loras", os.path.join(comfyui.folder_paths.get_output_directory(), "loras"))
+    folder_paths.add_model_folder_path("checkpoints", os.path.join(folder_paths.get_output_directory(), "checkpoints"))
+    folder_paths.add_model_folder_path("clip", os.path.join(folder_paths.get_output_directory(), "clip"))
+    folder_paths.add_model_folder_path("vae", os.path.join(folder_paths.get_output_directory(), "vae"))
+    folder_paths.add_model_folder_path("diffusion_models", os.path.join(folder_paths.get_output_directory(), "diffusion_models"))
+    folder_paths.add_model_folder_path("loras", os.path.join(folder_paths.get_output_directory(), "loras"))
 
     if args.input_directory:
         input_dir = os.path.abspath(args.input_directory)
         logging.info(f"Setting input directory to: {input_dir}")
-        comfyui.folder_paths.set_input_directory(input_dir)
-    
+        folder_paths.set_input_directory(input_dir)
+
     if args.user_directory:
         user_dir = os.path.abspath(args.user_directory)
         logging.info(f"Setting user directory to: {user_dir}")
-        comfyui.folder_paths.set_user_directory(user_dir)
+        folder_paths.set_user_directory(user_dir)
 
     if args.quick_test_for_ci:
         exit(0)
 
-    os.makedirs(comfyui.folder_paths.get_temp_directory(), exist_ok=True)
+    os.makedirs(folder_paths.get_temp_directory(), exist_ok=True)
     call_on_start = None
     if args.auto_launch:
         def startup_server(scheme, address, port):
@@ -262,9 +259,13 @@ if __name__ == "__main__":
         call_on_start = startup_server
 
     try:
-        loop.run_until_complete(comfyui.server.setup())
-        loop.run_until_complete(run(comfyui.server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
+        loop.run_until_complete(server.setup())
+        loop.run_until_complete(run(server, address=args.listen, port=args.port, verbose=not args.dont_print_server, call_on_start=call_on_start))
     except KeyboardInterrupt:
         logging.info("\nStopped server")
 
     cleanup_temp()
+
+
+if __name__ == "__main__":
+    main()
